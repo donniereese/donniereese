7c9f08f3092460a364bcbeab7d25b94d0cfb74a1
Donnie Reese <donniereese@gmail.com>
2018-02-17 17:10:45 -0600

    testing basic commands

--- js/term.js
+++ js/term.js
@@ -6,6 +6,11 @@ class WebTerm {
         // elements
         this.button = null;
         this.termWindow = null;
+        this.input = null;
+        this.buffer = null;
+        this.currentInput = '';
+
+        this.history = [];
 
         // Find button instances by class
         const buttonMatches = document.getElementsByClassName('termButton');
@@ -17,10 +22,53 @@ class WebTerm {
         this.button.addEventListener('click', this.toggleTerminal.bind(this));
     }
 
+    /**
+     * Escapes characters in the string that are not safe to use in a RegExp.
+     * @param {*} s The string to escape. If not a string, it will be casted
+     *      to one.
+     * @return {string} A RegExp safe, escaped copy of {@code s}.
+     * This is from Google library, so that's really cool.
+     */
+    regExpEscape(s) {
+        return String(s).replace(/([-()\[\]{}+?*.$\^|,:#<!\\])/g, '\\$1').
+            replace(/\x08/g, '\\x08');
+    };
+
     createNewButton() {
         const termButton = document.createElement('div');
         const buttonText = document.createTextNode('>');
         termButton.appendChild(buttonText);
+    }
+
+    command(e) {
+        e.preventDefault();
+        if (e.key === 'Enter') {
+            this.currentInput = this.regExpEscape(this.input.value);
+            const cmdArr = this.currentInput.split(' ');
+            let found = true;
+            switch(cmdArr[0]) {
+                case 'quit':
+                case 'exit':
+                    this.deactivateTerminal();
+                    break;
+                case 'reload':
+                case 'refresh':
+                    window.location.reload();
+                    break;
+                default:
+                    console.log('false!');
+                    found = false;
+            }
+
+            if (!found) {
+                const logItem =document.createElement('li');
+                const logText = document.createTextNode(`'${this.currentInput}' is not a valid command.`);
+                logItem.appendChild(logText);
+                this.log.appendChild(logItem);
+            }
+
+            this.input.value = '';
+        }
     }
 
     activateTerminal() {
@@ -28,12 +76,12 @@ class WebTerm {
         // create the terminal window
         this.termWindow = document.createElement('div');
         // create the input prompt
-        const inputPrompt = document.createElement('input');
+        this.input = document.createElement('input');
         // create the console log
-        const termLog = document.createElement('ul');
+        this.log = document.createElement('ul');
         // append them to the window element
-        this.termWindow.appendChild(termLog);
-        this.termWindow.appendChild(inputPrompt);
+        this.termWindow.appendChild(this.log);
+        this.termWindow.appendChild(this.input);
         // Give termWindow it's class
         this.termWindow.classList.add('termWindow');
         // toggle the sticky class on the button
@@ -41,6 +89,10 @@ class WebTerm {
         // add it to the document
         document.body.appendChild(this.termWindow);
         this.termWindow.appendChild(this.button);
+        // Set input event
+        this.input.addEventListener('keyup', this.command.bind(this));
+        // set focus on input
+        this.input.focus();
         console.log('activate');
 
         this.visible = true;
